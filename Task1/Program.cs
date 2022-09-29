8dd38cf52f38805d342bed8eb75e3953737553a6
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-29 23:02:08 +0300

    Добавление задания 1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Dishes) User;
+            User.Dishes = new string[5];
+
+            Console.Write("Введите имя: ");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Укажите пять своих любимых блюд");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{i + 1} блюдо: ");
+                User.Dishes[i] = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
