761980667611fd6c76db77cd047c0c37f84bf719
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-01 10:39:14 +0300

    Добавлено задание 5.3.1

---
+++ Task5.3.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task5_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свой возраст: ");
+            var Age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Возраст {Age}");
+            Console.ReadKey();
+        }
+        static void ChangeAge(int age)
+        {
+            age += 5; 
+        }
+    }
+}
