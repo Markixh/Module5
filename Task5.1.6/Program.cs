cfc1ca595c217c8550c475ef030256d362db2d41
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-01 10:28:43 +0300

    внесены изменения в задание 5.1.6 с учетом задания 5.2.18

--- Task5.1.6/Program.cs
+++ Task5.1.6/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            ShowArray(GetArrayFromConsole());   
+            ShowArray(GetArrayFromConsole(10), true);   
             Console.ReadKey();
         }
 
