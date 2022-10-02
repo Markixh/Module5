4235488c1fe7d836a18e843b746bac9a0a722998
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-02 15:51:56 +0300

    Добавлено задание 5.5.8, возведение в степень при помощи рекурсии

---
+++ Task5.5.8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task5_5_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerUp(2, 3));
+        }
+        private static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0) return 1;
+            else return N * PowerUp(N, --pow);
+        }
+    }
+}
